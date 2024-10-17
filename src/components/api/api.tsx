630ae237cd4310df0d50417ebeb1df3cda9c13e3
Denis Belokopytov <84543298+denis-heise@users.noreply.github.com>
2024-10-17 15:16:27 +0300

    Изменён адрес сервера

--- src/components/api/api.tsx
+++ src/components/api/api.tsx
@@ -1,7 +1,7 @@
 import axios, {AxiosInstance, AxiosRequestConfig} from 'axios';
 import { Token } from '../../utils/token';
 
-const BASE_URL = 'https://10.react.pages.academy/six-cities';
+const BASE_URL = 'https://10.react.htmlacademy.pro/six-cities';
 const REQUEST_TIMEOUT = 5000;
 
 export const createApi = (): AxiosInstance => {
